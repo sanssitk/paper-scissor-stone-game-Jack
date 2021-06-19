31d1cb7584d71fecd37197067ce8fa05daf14c02
sanjay <sanssitk@gmail.com>
2021-06-19 18:14:41 -0500

    button color selection

--- js/scripts.js
+++ js/scripts.js
@@ -84,10 +84,41 @@ rpsFrontEnd = (humanImagechoice, botImageChoice, finalMessage) => {
   document.getElementById("flex-box-rps-div").append(botDiv);
 };
 
-
 // Challenge : 4 --- Change the colors of buttons
 let all_buttons = document.getElementsByTagName("button");
 
-buttonChangeColor = (selected) => {
-
+let copyAllButtons = [];
+for (button of all_buttons) {
+  copyAllButtons.push(button.classList[1]);
 }
+console.log(copyAllButtons);
+buttonChangeColor = (buttonThingy) => {
+  if (buttonThingy.value === "red") {
+    buttonsRed();
+  } else if (buttonThingy.value === "green") {
+    buttonsGreen();
+  } else if (buttonThingy.value === "reset") {
+    buttonColorReset();
+  } else {
+    randomColors();
+  }
+};
+
+buttonsRed = () => {
+  for (allbotton of all_buttons) {
+    allbotton.classList.remove(allbotton.classList[1]);
+    allbotton.classList.add("btn-danger");
+  }
+};
+buttonsGreen = () => {
+  for (allbotton of all_buttons) {
+    allbotton.classList.remove(allbotton.classList[1]);
+    allbotton.classList.add("btn-success");
+  }
+};
+buttonColorReset = () => {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add(copyAllButtons[i]);
+  }
+};
